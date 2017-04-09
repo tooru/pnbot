ab6b68aacfd99a60b421c148b842153a03b13771
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-04-10 00:30:37 +0900

    Modified prime.go and prime_test.go.

--- prime.go
+++ prime.go
@@ -1,35 +1,86 @@
 package pnbot
 
 import (
+    "fmt"
     "math/big"
+    "sync"
 )
 
 var big0 = big.NewInt(0)
 var big2 = big.NewInt(2)
-var primes = []*big.Int{big2}
+var big3 = big.NewInt(3)
 
-func IsPrime(n *big.Int) bool {
-    if (n.Cmp(big2) < 0) {
-        return false
+var primes = []*big.Int{big2, big3}
+var pMutex = new(sync.Mutex)
+
+type Primes struct {
+    i int // TODO more scalable
+}
+
+// error will be IO or timeout
+func IsPrime(n *big.Int) (bool, error) {
+    if n.Cmp(big2) < 0 {
+        return false, nil
     }
 
     r := new(big.Int)
     r.Sqrt(n)
 
-    for p := primes[len(primes)-1]; p.Cmp(r) <= 0; p.Add(p, big2) {
-        if IsPrime(p) {
-            primes = append(primes, p)
-        }
-    }
+    fmt.Printf("IsPrime: %v/%v/%v\n", n, r, primes)
 
-    for _, p := range primes {
+    ps := new(Primes)
+
+    for {
+        p, err := ps.Next()
+        if err != nil {
+            return false, err
+        }
+        fmt.Printf("IsPrime: %v/%v/%v/%v\n", n, r, p, primes)
+
         if p.Cmp(r) > 0 {
             break
         }
         m := new(big.Int)
         if m.Mod(n, p).Cmp(big0) == 0 {
-            return false
+            return false, nil
         }
     }
-    return true
+    return true, nil
+}
+
+func (ps *Primes) Next() (*big.Int, error) {
+    //pMutex.Lock() // TODO optimize
+    //defer pMutex.Unlock()
+
+    fmt.Printf("Next:%v/%v\n", ps.i, primes)
+
+    if ps.i < len(primes) {
+        i := ps.i
+        ps.i++
+        return newInt(primes[i]), nil
+    }
+
+    p := newInt(primes[len(primes)-1])
+
+    for {
+        p.Add(p, big2)
+
+        b, err := IsPrime(p)
+        if err != nil {
+            return nil, err
+        }
+        if b {
+            primes = append(primes, newInt(p))
+
+            ps.i++
+            return newInt(p), nil
+        }
+    }
+}
+
+func newInt(i *big.Int) *big.Int {
+    bi := new(big.Int)
+    bi.Set(i)
+
+    return bi
 }
