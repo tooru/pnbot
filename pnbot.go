a6c40a0d4bcd8bff62d660c07d426bc8b3dcfe6b
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-05-23 23:28:56 +0900

    Added .gitignore. Modified pnbot.go.

--- pnbot.go
+++ pnbot.go
@@ -2,11 +2,12 @@ package main
 
 import (
     "flag"
-    "fmt"
     "log"
     "os"
+    "math/big"
+    "time"
 
-//    "github.com/tooru/pnbot/prime"
+    "github.com/tooru/pnbot/prime"
     "github.com/dghubble/go-twitter/twitter"
     "github.com/dghubble/oauth1"
 )
@@ -28,16 +29,93 @@ func main() {
     config := oauth1.NewConfig(*consumerKey, *consumerSecret)
     token := oauth1.NewToken(*accessToken, *accessSecret)
 
-    tweet(config, token, "hello twitter")
-}
-
-func tweet(config *oauth1.Config, token *oauth1.Token, status string) {
     httpClient := config.Client(oauth1.NoContext, token)
     client := twitter.NewClient(httpClient)
-    tweet, _, err := client.Statuses.Update(status, nil)
+
+    
+    primes := prime.NewPrime()
+
+    pnTweet(client, primes)
+}
+
+func pnTweet(client *twitter.Client, primes *prime.Prime) {
+    maxPrime, err := getMaxPrime(client)
     if err != nil {
-        log.Fatalf("Tweet error: %v\n", err)
+        log.Fatalf("getMaxPrime: %v\n", err)
         return
     }
-    fmt.Println(tweet)
+
+    ch := make(chan *big.Int, 1000)
+
+    go makePrimes(primes, maxPrime, ch)
+    tweetPrimes(client, ch)
+}
+
+func getMaxPrime(client *twitter.Client) (prime *big.Int, err error) {
+    tweets, _, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
+        SinceID: 0,
+    })
+    if err != nil {
+        return nil, err
+    }
+
+    var maxPrime big.Int
+
+    for _, tweet := range tweets {
+        if _, ok := maxPrime.SetString(tweet.Text, 10); ok {
+            log.Printf("continue from '%s'", tweet.Text)
+            return &maxPrime, nil
+        }
+        log.Printf("skip '%v'", tweet.Text)
+    }
+    maxPrime.SetInt64(0)
+    return &maxPrime, nil
+}
+
+func makePrimes(primes *prime.Prime, maxPrime *big.Int, ch chan *big.Int) {
+    var prime *big.Int
+    var err error
+
+    for {
+        prime, err = primes.Next()
+        if err != nil {
+            log.Fatalf("makePrimes: %v\n", err)
+            ch <- nil
+            return
+        }            
+
+        if prime.Cmp(maxPrime) > 0 {
+            break;
+        }
+    }
+
+    for {
+        ch <- prime
+
+        prime, err = primes.Next()
+        if err != nil {
+            log.Fatalf("makePrimes: %v\n", err)
+            ch <- nil
+            return
+        }            
+    }
+}
+
+func tweetPrimes(client *twitter.Client, ch chan *big.Int) {
+    for {
+        prime := <- ch
+        if prime == nil {
+            log.Fatalf("prime generator was died")
+            return
+        }
+
+        text := prime.Text(10)
+        _, _, err := client.Statuses.Update(text, nil)
+        if err != nil {
+            log.Fatalf("Tweet error: %v\n", err)
+            return
+        }
+        log.Printf("tweet %s\n", text)
+        time.Sleep(30 * time.Second)
+    }
 }
