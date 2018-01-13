97c84e4cb88de0e27b59c0d872f64bd75130e76f
Tooru Takahashi <tooru.takahashi134@gmail.com>
2018-01-14 00:33:34 +0900

    gofmt slide/tweet.go

--- slide/tweet.go
+++ slide/tweet.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "github.com/dghubble/go-twitter/twitter"
-    "github.com/dghubble/oauth1"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 const (
-    consumerKey = "" // ⚠️
-    consumerSecret = "" // ⚠️
-    accessToken = "" // ⚠️
-    accessSecret = "" // ⚠️
-)    
+	consumerKey    = "" // ⚠️
+	consumerSecret = "" // ⚠️
+	accessToken    = "" // ⚠️
+	accessSecret   = "" // ⚠️
+)
 
 func main() {
-    config := oauth1.NewConfig(consumerKey, consumerSecret)
-    token := oauth1.NewToken(accessToken, accessSecret)
-    httpClient := config.Client(oauth1.NoContext, token)
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
 
-    client := twitter.NewClient(httpClient)
-    client.Statuses.Update("Hello Twitter", nil) // ここを繰り返す
+	client := twitter.NewClient(httpClient)
+	client.Statuses.Update("Hello Twitter", nil) // ここを繰り返す
 }
